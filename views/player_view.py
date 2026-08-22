d707f61d2a9939694184801801a9a695a619dcf7
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-08-22 19:27:56 -0300

    Update player_view.py

--- views/player_view.py
+++ views/player_view.py
@@ -3,10 +3,10 @@ from core.history import HistoryManager
 
 class PlayerView:
     @staticmethod
-    def build(page: ft.Page, video_path: str, ep_title: str, on_back):
+    def build(page: ft.Page, video_path: str, ep_title: str, on_back, on_next_episode=None):
         prog = HistoryManager.get_progress_data(video_path)
 
-        def save_and_exit():
+        def save_and_exit(go_to_next=False):
             try:
                 current_pos = video_player.get_current_position()
                 duration = video_player.get_duration()
@@ -18,18 +18,29 @@ class PlayerView:
                     HistoryManager.save_position(video_path, pos_sec, dur_sec)
             except Exception:
                 pass
-            on_back()
+
+            if go_to_next and on_next_episode:
+                on_next_episode()
+            else:
+                on_back()
 
         back_button = ft.IconButton(
             icon=ft.Icons.ARROW_BACK,
             icon_color=ft.Colors.WHITE,
-            on_click=lambda _: save_and_exit()
+            on_click=lambda _: save_and_exit(go_to_next=False)
         )
 
+        next_button = ft.IconButton(
+            icon=ft.Icons.SKIP_NEXT,
+            icon_color=ft.Colors.WHITE,
+            tooltip="Próximo Episódio",
+            on_click=lambda _: save_and_exit(go_to_next=True)
+        ) if on_next_episode else ft.Container()
+
         header = ft.Row([
-            back_button,
-            ft.Text(ep_title, size=15, weight=ft.FontWeight.BOLD, color=ft.Colors.WHITE)
-        ], alignment=ft.MainAxisAlignment.START)
+            ft.Row([back_button, ft.Text(ep_title, size=15, weight=ft.FontWeight.BOLD, color=ft.Colors.WHITE)]),
+            next_button
+        ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
 
         video_player = ft.Video(
             playlist=[ft.VideoMedia(video_path)],

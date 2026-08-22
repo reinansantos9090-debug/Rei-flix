357765a6c428e5f1d288f8f2c129d8eff26c596c
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-08-22 19:15:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import flet as ft
-import os
+from views.home_view import HomeView
+from views.details_view import DetailView
+from views.player_view import PlayerView
 
 def main(page: ft.Page):
     page.title = "Rei-Flix Local"
     page.theme_mode = ft.ThemeMode.DARK
     page.bgcolor = ft.Colors.BLACK
-    
-    # Caminho padrao da pasta de animes no Android
-    ANIME_DIR = "/storage/emulated/0/Download/Animes"
-    
-    if not os.path.exists(ANIME_DIR):
-        page.add(ft.Text(f"Pasta não encontrada: {ANIME_DIR}", color=ft.Colors.RED_ACCENT))
-    else:
-        folders = os.listdir(ANIME_DIR)
-        page.add(ft.Text(f"Animes encontrados no celular: {len(folders)}", color=ft.Colors.GREEN_ACCENT))
+    page.padding = 0
+
+    current_anime = [None]
+
+    def play_episode(video_path, ep_title):
+        page.clean()
+        player_ui = PlayerView.build(
+            page, 
+            video_path=video_path, 
+            ep_title=ep_title, 
+            on_back=lambda: navigate_to_details(current_anime[0])
+        )
+        page.add(player_ui)
+        page.update()
+
+    def navigate_to_details(anime):
+        current_anime[0] = anime
+        page.clean()
+        detail_ui = DetailView.build(
+            page, 
+            anime_data=anime, 
+            on_play_episode=play_episode, 
+            on_back=navigate_to_home
+        )
+        page.add(detail_ui)
+        page.update()
+
+    def navigate_to_home():
+        page.clean()
+        home_ui = HomeView.build(page, on_select_anime=navigate_to_details)
+        page.add(home_ui)
+        page.update()
+
+    navigate_to_home()
 
 ft.app(target=main)

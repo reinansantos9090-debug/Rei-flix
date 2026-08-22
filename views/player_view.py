1949334934893e91d1fa77d4a99a76a039b918db
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-08-22 19:12:22 -0300

    Update player_view.py

--- views/player_view.py
+++ views/player_view.py
@@ -1,12 +1,26 @@
 import flet as ft
+from core.history import HistoryManager
 
 class PlayerView:
     @staticmethod
-    def build(page: ft.Page, video_url: str, ep_title: str, on_back):
+    def build(page: ft.Page, video_path: str, ep_title: str, on_back):
+        # Recupera os segundos onde o usuario parou da ultima vez
+        saved_position = HistoryManager.get_position(video_path)
+
+        def save_and_exit():
+            # Salva a posicao atual do video ao sair da tela
+            try:
+                current_pos = video_player.get_current_position()
+                if current_pos:
+                    HistoryManager.save_position(video_path, current_pos.total_seconds())
+            except Exception:
+                pass
+            on_back()
+
         back_button = ft.IconButton(
             icon=ft.Icons.ARROW_BACK,
             icon_color=ft.Colors.WHITE,
-            on_click=lambda _: on_back()
+            on_click=lambda _: save_and_exit()
         )
 
         header = ft.Row([
@@ -14,10 +28,10 @@ class PlayerView:
             ft.Text(ep_title, size=16, weight=ft.FontWeight.BOLD, color=ft.Colors.WHITE)
         ], alignment=ft.MainAxisAlignment.START)
 
-        # Componente Nativo do Flet para Reprodução de Vídeo
+        # Componente de Video nativo apontando para o arquivo local do celular
         video_player = ft.Video(
-            playlist=[ft.VideoMedia(video_url)],
-            playlist_mode=ft.PlaylistMode.LOOP,
+            playlist=[ft.VideoMedia(video_path)],
+            playlist_mode=ft.PlaylistMode.NONE,
             fill_color=ft.Colors.BLACK,
             aspect_ratio=16/9,
             autoplay=True,
@@ -26,7 +40,7 @@ class PlayerView:
 
         layout = ft.Column([
             header,
-            ft.Divider(height=20, color=ft.Colors.TRANSPARENT),
+            ft.Divider(height=10, color=ft.Colors.TRANSPARENT),
             ft.Container(
                 content=video_player,
                 alignment=ft.alignment.center,

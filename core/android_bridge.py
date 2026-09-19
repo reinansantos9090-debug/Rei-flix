fe8e02ebfaaf0196acee6eb0b2c5bb5b69e433ab
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-19 14:40:38 -0300

    feat(bridge): add MediaStore scan command

--- core/android_bridge.py
+++ core/android_bridge.py
@@ -47,6 +47,7 @@ class AndroidBridge:
 
     async def select_tree(self): await self._launch("select_tree")
     async def rescan_tree(self, tree_uri: str): await self._launch("scan_tree", tree_uri=tree_uri)
+    async def scan_media_store(self): await self._launch("scan_media_store")
     async def verify_tree(self, tree_uri: str): await self._launch("verify_tree", tree_uri=tree_uri)
     async def release_tree(self, tree_uri: str): await self._launch("release_tree", tree_uri=tree_uri)
     async def sign_in(self, server_client_id: str): await self._launch("google_sign_in", server_client_id=server_client_id)

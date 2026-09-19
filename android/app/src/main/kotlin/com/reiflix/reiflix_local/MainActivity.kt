fd53034ce523aba77aa912bb15bcb9eaf9e75f38
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-19 04:54:34 -0300

    fix: use FlutterFragmentActivity for Android activity result API

--- android/app/src/main/kotlin/com/reiflix/reiflix_local/MainActivity.kt
+++ android/app/src/main/kotlin/com/reiflix/reiflix_local/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -20,7 +20,7 @@ import org.json.JSONObject
  * FlutterActivity. It owns SAF and Google identity because only Android has a
  * ContentResolver/Credential Manager.
  */
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val tag = "[REIFLIX][ANDROID]"
     private val treePicker = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
         handleTreePickerResult(result)

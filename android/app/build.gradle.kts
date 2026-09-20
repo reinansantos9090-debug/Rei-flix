b18ca2b99f4f348f4a448d9d6ea52a090d2df2ce
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-20 16:51:35 -0300

    build: add Kotlin unit and mailbox test dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,4 +32,9 @@ dependencies {
     implementation("androidx.credentials:credentials:1.6.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.6.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.2.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test:core:1.6.1")
 }

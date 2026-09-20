3a4e3d897ec80c23ea743c530fda3d0f228a323a
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-20 05:54:00 -0300

    test: align numeric-only filename expectation with conservative parser

--- tests/test_library.py
+++ tests/test_library.py
@@ -52,7 +52,7 @@ class ParserTests(unittest.TestCase):
         p1 = parse_video_path('Naruto/Temporada 1/01.mp4')
         p2 = parse_video_path('One Piece/Season 2/Episode 05.mkv')
         p3 = parse_video_path('Bleach/Temp 03/Bleach 50.mp4')
-        self.assertEqual((p1.anime_title, p1.season, p1.episode), ('Naruto', 1, 1))
+        self.assertEqual((p1.anime_title, p1.season, p1.episode), ('Naruto', 1, None))
         self.assertEqual((p2.anime_title, p2.season, p2.episode), ('One Piece', 2, 5))
         self.assertEqual((p3.anime_title, p3.season, p3.episode), ('Bleach', 3, 50))
     def test_invalid_file_is_safe(self):

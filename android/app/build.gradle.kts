5272bb5eea4793ef3716810207efbdafa49c3539
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-20 16:58:12 -0300

    test: declare AndroidJUnitRunner for instrumentation coverage

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "0.2.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17

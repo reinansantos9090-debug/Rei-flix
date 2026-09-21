e54e9410987f847d8a5919a8888430f365f83c72
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-21 11:35:06 -0300

    test(index): verify cross-source primary identity normalization

--- android/app/src/test/kotlin/com/reiflix/reiflix_local/NativeIndexTest.kt
+++ android/app/src/test/kotlin/com/reiflix/reiflix_local/NativeIndexTest.kt
@@ -24,6 +24,33 @@ class NativeIndexTest {
     }
 
     @Test
+    fun safPrimaryIdentityConvergesWithMediaStoreExternalPrimary() {
+        val saf = JSONObject()
+            .put("treeUri", "content://com.android.externalstorage.documents/tree/primary%3AMovies")
+            .put("documentId", "primary:Movies/Show/a.mkv")
+            .put("volumeId", "primary")
+            .put("relativePath", "Show/a.mkv")
+        val broad = JSONObject()
+            .put("uri", "file:///storage/emulated/0/Movies/Show/a.mkv")
+            .put("volumeId", "external_primary")
+            .put("relativePath", "Movies/Show/a.mkv")
+        assertEquals(
+            NativeIndex.stableIdentity(saf, NativeIndex.SOURCE_SAF),
+            NativeIndex.stableIdentity(broad, NativeIndex.SOURCE_BROAD),
+        )
+    }
+
+    @Test
+    fun distinctVolumesWithSameRelativePathStayDistinct() {
+        val primary = JSONObject().put("volumeId", "external_primary").put("relativePath", "Movies/a.mkv")
+        val sd = JSONObject().put("volumeId", "ABCD-1234").put("relativePath", "Movies/a.mkv")
+        assertNotEquals(
+            NativeIndex.stableIdentity(primary, NativeIndex.SOURCE_BROAD),
+            NativeIndex.stableIdentity(sd, NativeIndex.SOURCE_BROAD),
+        )
+    }
+
+    @Test
     fun stableIdentity_doesNotUseDisplayNameAlone() {
         val a = JSONObject().put("uri", "content://media/1").put("volumeId", "external_primary").put("relativePath", "A/a.mkv").put("name", "a.mkv")
         val b = JSONObject().put("uri", "content://media/2").put("volumeId", "external_primary").put("relativePath", "B/a.mkv").put("name", "a.mkv")

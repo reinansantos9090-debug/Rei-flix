b7a6f60d8f7f80af4db2ac04e9131b136e6da4cb
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-21 19:10:52 -0300

    chore: bump Android version baseline for installable updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.reiflix.reiflix_local"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.2.0"
+        versionCode = 2
+        versionName = "0.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {

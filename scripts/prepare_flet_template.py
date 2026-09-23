3f803815891e444f580b3f70f24be32d597b8379
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-23 05:54:31 -0300

    test(android): remove unused UiAutomator dependency from host template

--- scripts/prepare_flet_template.py
+++ scripts/prepare_flet_template.py
@@ -275,7 +275,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test:core:1.6.1")
-    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
 }
 """
     else:

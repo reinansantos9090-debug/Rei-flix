9b152bf8384ad256fbc99804a844d16dbb6be5a0
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-09-23 05:39:16 -0300

    test(android): carry UiAutomator into rendered Flet host

--- scripts/prepare_flet_template.py
+++ scripts/prepare_flet_template.py
@@ -275,6 +275,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test:core:1.6.1")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
 }
 """
     else:
